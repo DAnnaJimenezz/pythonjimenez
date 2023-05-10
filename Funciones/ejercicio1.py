cc045ac7368f34b4d6eb212ca5fd3e08bdcf7c8b
Danna <jimenezcastrodanna6@gmail.com>
2023-05-10 17:06:58 -0500

    Actualizacion

--- Funciones/ejercicio1.py
+++ Funciones/ejercicio1.py
@@ -15,41 +15,25 @@ def promedioLista(lista):
     return sumaLista(lista)/len(lista)
 
 def mayorLista(lista):
-    if l1 > lista:
-        mayorLista = l1
-    if l2 > lista:
-        mayorLista = l2
-    if l3 > lista:
-        mayorLista = l3
+    for i in lista:
+        if i > lista:
+            mayorLista = i
+    return mayorLista
         
 def menorLista(lista):
-    if l1 < lista:
-        menorLista = l1
-    if l2 < lista:
-        menorLista = l2
-    if l3 < lista:
-        menorLista = l3
-
+    for i in lista:
+        if i < lista:
+            menorLista = i
+    return menorLista
         
-l1=llenarLista(20,35)
-l2=llenarLista(10,60)
-l3=llenarLista(0,100)
+l1=llenarLista(35,50)
 print(l1)
-print(l2)
-print(l3)
 
 print(sumaLista(l1))
-print(sumaLista(l2))
-print(sumaLista(l3))
 
 print(promedioLista(l1))
-print(promedioLista(l2))
-print(promedioLista(l3))
 
 print(mayorLista(l1))
-print(mayorLista(l2))
-print(mayorLista(l3))
 
 print(menorLista(l1))
-print(menorLista(l2))
-print(menorLista(l3))
+
