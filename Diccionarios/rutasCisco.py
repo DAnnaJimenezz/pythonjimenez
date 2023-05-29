8695a32ecca3978c47c3369d5564eb1ce2ec00de
Danna <jimenezcastrodanna6@gmail.com>
2023-05-29 17:05:26 -0500

    CLase 05/29/23

---
+++ Diccionarios/rutasCisco.py
@@ -0,0 +1,10 @@
+import sys 
+from sys import path 
+path.append ('C:\\Users\APRENDIZ SENA\Downloads\Archivo ZIP Módulos y Paquetes1\extra\good')
+
+import extra.iota
+import extra.good.alpha 
+print(extra.iota.FunI())
+print(extra.good.alpha.FunA)
+
+
