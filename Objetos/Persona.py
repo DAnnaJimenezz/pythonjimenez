6a1d38ce94d90696d96ff47cc23d81f3701cf65c
Danna <jimenezcastrodanna6@gmail.com>
2023-06-04 17:59:33 -0500

    Tareita, no pude solucionarle el error :/

--- Objetos/Persona.py
+++ Objetos/Persona.py
@@ -1,27 +1,43 @@
 class Persona:
     def __init__(self, nombre, documento):
-        print (int(input("Ingrese un curso:")))
         self.__nombre=nombre
         self.__documento=documento
         self.__cursos=[]
         
     def getNombre(self):
         return self.__nombre
-    def getNombre(self, nombre):
+    def setNombre(self, nombre):
         self.__nombre=nombre 
+
     def getDocumento(self):
         return self.__documento
-    def getDocumento(self, documento):
+    def setDocumento(self, documento):
         self.__documento=documento
+    
     def getCursos(self):
         return self.__cursos
-    def getCursos(self,cursos):
+    def setCursos(self,cursos):
         self.__cursos=cursos
-        
-    def __init__(self, ciudad):
-        self.__ciudad=ciudad
-        
-    def getCiudad(self):
-        return self.__ciudad
-    def getCiudad(self,ciudad):
-        self.__ciudad=ciudad
+    
+    def agregarCursos(self):
+        curso=input("Ingrese el nombre del curso:")
+        self.cursos.append(curso)
+
+    def getDatos(self):
+        return f'{self.__nombre}, {self.__documento}'
+
+
+p=Persona('Ana', 123,)
+print(p.getNombre())
+q=Persona('Pedro',321,)
+print(q.getNombre())
+
+p.getDatos()
+print(p.getDatos())
+q.getDatos()
+print(q.getDatos())
+
+p.agregarCursos()
+print(p.getCursos())
+q.agregarCursos()
+print(q.getCursos())
