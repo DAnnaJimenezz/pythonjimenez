6229b3cfabf02312afcf79a8acb5565caf547d30
Danna <jimenezcastrodanna6@gmail.com>
2023-08-01 19:28:08 -0500

    Actualizacion 2 Tareita =D

--- ArchivosPython/Archivo3/datosPersonales.py
+++ ArchivosPython/Archivo3/datosPersonales.py
@@ -1,16 +1,22 @@
 import os 
-def ValidaciondeArchivo (funcionArchivo):
+def DatosPerosnales (funcionArchivo, datos):
     try:
-        nombre = input("Ingrese su nombre:")
-        apellido = input("Ingrese su apellido:")
-        edad = int(input("Ingrese su edad:"))
-        email = input("Ingrese su correo electronico:")
-        with open (funcionArchivo,"w+") as daticosPersonales:
-            print ("Datos personales agregados correctamente")
-        daticosPersonales.close()
+        archivo = open(funcionArchivo, "a")
+        for i in datos:
+            archivo.write(i + '\n')
+        archivo.close()
+        print ("Los datos fueron agregados correctamente")
     except IOError:
         print ("Se produjo un error")
-        
-#funcionArchivo = "C:\\pythonjimenez\\Archivos\\funcionPrueba.txt"
+
+nombre = input("Ingrese su nombre:")
+apellido = input("Ingrese su apellido:")
+genero = input("Ingrese su genero:")
+edad = input("Ingrese su edad:")
+email = input("Ingrese su correo electronico:")
+deporteFavorito = input("Ingrese su deporte favorito:")
+generoDeMusica = input("Ingrese su genero favorito de musica:")
+datos = (nombre, apellido, genero, edad, email, deporteFavorito, generoDeMusica)
+
 funcionArchivo = "C:\\clon\\pythonjimenez\\ArchivosPython\\Archivo3\\daticosPersonales.txt"
-ValidaciondeArchivo (funcionArchivo)
+DatosPerosnales (funcionArchivo, datos)
